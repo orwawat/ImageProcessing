0727e5e4a9a61dac5c51da9c35e025d6a1339c39
yuvaljacoby <yuval999@gmail.com>
2016-11-16 18:06:46 +0200

    work on 3.4

--- EX1/sol1.py
+++ EX1/sol1.py
@@ -33,6 +33,7 @@ class RgbYiqConverter:
         rgb = np.dot(np.linalg.inv(RgbYiqConverter.yiqToRgb), imgToConvert)
         rgb = rgb.T
         rgb = rgb.reshape((yiqImg.shape[0], yiqImg.shape[1], yiqImg.shape[2]))
+        # TODO: return array in [0,255]?
         return rgb.astype(np.float32)
 
 
@@ -45,13 +46,13 @@ def getGrayImage(im_float):
     :return:
     '''
     if isGraySacle(im_float):
+        # Already gray, no need to convert
+        grayImage = im_float
+    else:
         # Convert to gray scale
         im_gray = color.rgb2gray(im_float)
         im_gray = im_gray.astype(np.float32)
         grayImage = im_gray
-    else:
-        # Already gray, no need to convert
-        grayImage = im_float
 
     return grayImage
 
@@ -63,17 +64,21 @@ def read_image(filename, representation):
 
     if representation == 1:
         # Convert to gray
-        getGrayImage(im_float)
+        returnImage = getGrayImage(im_float)
     else:
         returnImage = im_float
 
     return returnImage
 
 def imdisplay(filename, representation):
+    arrdisplay(read_image(filename, representation))
+
+
+def arrdisplay(im, representation):
     if representation == 1:
-        plt.imshow(read_image(filename, representation), plt.cm.gray)
+        plt.imshow(im, plt.cm.gray)
     else:
-        plt.imshow(read_image(filename, representation))
+        plt.imshow(im)
     plt.show()
 
 def rgb2yiq(imRGB):
@@ -83,46 +88,92 @@ def yiq2rgb(imYIQ):
     return RgbYiqConverter.getRGB(imYIQ)
 
 
-def histogram_equalize(im_orig):
-    ''' Algorithm:
-    1. Given image I(x,y), create a histogram 
-    H:
-    • For all x,y:  H(I(x,y)) = H(I(x,y)) + 1
-    2. Create cumulative histogram S(k):
-    • S(0) = H(0);  S(k+1) = S(k) + H(k+1); 
-    • Let m be first grey level for which S(m)≠0;
-    3. Create Look Up Table (LUT) T(k):
-    • T(k) = round{255 × [S(k)‐S(m)] / [S(255)‐S(m)] }
-    4. Apply LUT 
-    T to image I, get equalized image 
-    J
-    • J(x,y) = T (I(x,y))
-    '''
+def histogram_equalize1(im_orig):
 
     if not isGraySacle(im_orig):
         im = yiq2rgb(im_orig)[:, :, 0]
     else:
         im = im_orig
 
-    H = im.histogram()
+    im = im * 256
+    hist_orig, bin_edges = np.histogram(im, bins=256, range=(0, 256))
+    # print("histogram is: ", hist_orig)
+    # print("\n\n****************************\n\n")
+    sumHist = np.cumsum(hist_orig, dtype=np.float32)
+    # print("sumHist is: ", sumHist)
+    # print("\n\n****************************\n\n")
+
+    minGray = sumHist.item(np.nonzero(sumHist)[-1][-1])
+    print(minGray)
+
+    cdf_m = np.ma.masked_equal(sumHist, 0)
+    cdf_m = (cdf_m - cdf_m.min()) * 255 / (255 - cdf_m.min())
+    lut = np.ma.filled(cdf_m, 0).astype('uint8')
+
+    '''
+    lut = np.ndarray(shape=(256))
+
+    # print(sumHist[minGray])
+
+    lut = np.round(255 * (sumHist - minGray) / sumHist.item(255) - minGray)
+
+    for i in range(0, 256):
+        print(np.round(255 * (sumHist[i] - sumHist[minGray]) / sumHist[255] - sumHist[minGray]))
+        lut.itemset(i, np.round(255 * (sumHist[i] - sumHist[minGray]) / sumHist[255] - sumHist[minGray]))
+        '''
+
+    print(lut)
+    im_eq = lut[im]
+
+    hist_eq = np.histogram(im_eq, bins=256, range=(0,256))
+    if not isGraySacle(im_orig):
+        im_eq = yiq2rgb(im_eq)
+
+    #print("The sum histogram is: ", sumHist)
 
     return [im_eq, hist_orig, hist_eq]
-myPic = read_image('color.jpg', 2)
-myPic = myPic#[0:2,0:2,:].round(3)
+
+
+def histogram_qualize(im_orig):
+    if not isGraySacle(im_orig):
+        im = yiq2rgb(im_orig)[:, :, 0]
+    else:
+        im = im_orig
+
+    im = im * 256
+    hist_orig, bin_edges = np.histogram(im, bins=256, range=(0, 256))
+    sumHist = np.cumsum(hist_orig, dtype=np.float32)
+
+    m = 0
+    while sumHist.item(m) != 0:
+        m += 1
+
+    lut = np.ndarray(shape=(256))
+
+    for i in range(0,256):
+        lut.setitem(i, np.round(255 * (sumHist.item(i) - sumHist(m)) / (sumHist.item(255) - sumHist(m))))
+
+    im_eq = lut[im]
+    return [im_eq, hist_orig, hist_eq]
+
+myPic = read_image('bw.jpg', 1)
+#myPic = myPic[0:2,0:2,:].round(3)
+myPic, hist_orig, hist_eq = histogram_equalize(myPic)
+arrdisplay(myPic, 1)
 #print("myPic is: \n")
 #print(myPic)
 #print("\n\n")
 
-yiqPic = rgb2yiq(myPic)
-print("first pic some pixel value: ", myPic[95, 60, :], "\n\n")
-print("yiq pic some pixel value: ", yiqPic[95, 60, :], "\n\n")
-rgb = yiq2rgb(yiqPic)
-print("rgb pic some pixel value: ", rgb[95, 60, :], "\n\n")
+# yiqPic = rgb2yiq(myPic)
+# print("first pic some pixel value: ", myPic[95, 60, :], "\n\n")
+# print("yiq pic some pixel value: ", yiqPic[95, 60, :], "\n\n")
+# rgb = yiq2rgb(yiqPic)
+# print("rgb pic some pixel value: ", rgb[95, 60, :], "\n\n")
 #print("********************************")
 #print("myPic YIQ pic: \n")
 #print(yiqPic)
 
-plt.imshow(rgb)
+
 #plt.imshow(yiqPic[:, :, 0], plt.cm.gray)
-plt.show()
+
 #imdisplay('color.jpg', 1)
